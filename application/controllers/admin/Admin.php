01a98e5620e223e827cdc84849d0e7e117dd37b6
AMIT ANAND <amitanand.kvj.2@gmail.com>
2018-01-24 03:09:17 +0530

    admin1

--- application/controllers/admin/Admin.php
+++ application/controllers/admin/Admin.php
@@ -31,4 +31,83 @@ class Admin extends CI_Controller {
 		$this->load->view('portfoliodetails');
 		$this->load->view('footer');
 	}
+	public function client()
+	{
+		$config = array();
+        $config["base_url"] = base_url() . "index.php/admin/client";
+        $config["total_rows"] = $this->user->countclient();
+        $config["per_page"] = 50;
+        $config["uri_segment"] = 3;
+
+        $this->pagination->initialize($config);
+
+        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+        $details['query'] = $this->user->get_client($config["per_page"], $page);
+        $details["links"] = $this->pagination->create_links();
+        $this->load->view('header');
+		$this->load->view('viewuser',$details);
+		$this->load->view('footer');
+	}
+    public function scategory()
+	{	$this->form_validation->set_rules('name', 'name', 'required');
+		if ($this->form_validation->run() == FALSE)
+        {
+        	$details['query']=$this->user->showcategory();
+     		$this->load->view('header');
+		$this->load->view('scategory',$details);
+		$this->load->view('footer');
+        }
+		else
+		{
+			
+			$name=$_POST['name'];
+		    $descr=$_POST['descr'];
+
+		    $categorys = $_POST['category'];
+
+
+					    
+			
+			$result=$this->user->insert_scategory( $name,$descr,$categorys);
+		if ($result)
+			{
+				$this->session->set_flashdata('msg','<div class="alert alert-success text-center"> Successfully Updated</div>');
+				redirect('admin/scategory');
+			}
+			else
+			{
+				// error
+				$this->session->set_flashdata('msg','<div class="alert alert-danger text-center">Oops! Error.  Something Went Wrong</div>');
+				redirect('admin/scategory');
+			}
+		}
+	
+		
+	}
+	public function Deletescategory($id)
+	{
+			
+		$details['query']=$this->user->showscategory();
+     		$this->load->view('header');
+		$this->load->view('scategory',$details);
+		$this->load->view('footer');
+	  
+	  echo "<script>
+	 x = confirm ('You want to proceed deleting?')";
+	 
+	  $r=$this->user->deletescategory($id);
+	  if($r){
+	  echo "Successfully Deleted Data";
+	  }
+	  else {
+		  echo "Can Not Delete Data";
+	  }
+	  
+	   
+	  
+	  redirect('admin/scategory');
+	 
+	}
+
+	
 }
--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-  <title>SB Admin - Start Bootstrap Template</title>
+  <title>99-WORKERS</title>
   <!-- Bootstrap core CSS-->
   <link href="<?php echo base_url();?>mediaadmin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom fonts for this template-->
@@ -21,7 +21,7 @@
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
   <!-- Navigation-->
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+    <a class="navbar-brand" href="index.html"><img src="<?php echo base_url();?>media/images/logow.png" class="img-responsive" style="height: 30px;"></a>
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -34,22 +34,8 @@
           </a>
         </li>
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
-          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseUsers" data-parent="#exampleAccordion">
-            <i class="fa fa-fw fa-wrench"></i>
-            <span class="nav-link-text">Users</span>
-          </a>
-          <ul class="sidenav-second-level collapse" id="collapseUsers">
-            <li>
-              <a href="navbar.html">All User</a>
-            </li>
-            <li>
-              <a href="cards.html">Suspended User</a>
-            </li>
-          </ul>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
           <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
-            <i class="fa fa-fw fa-wrench"></i>
+            <i class="fa fa-align-left" aria-hidden="true"></i>
             <span class="nav-link-text">Projects</span>
           </a>
           <ul class="sidenav-second-level collapse" id="collapseComponents">
@@ -64,9 +50,58 @@
             </li>
           </ul>
         </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Email">
+          <a class="nav-link" href="index.html">
+            <i class="fa fa-envelope-o" aria-hidden="true"></i>
+            <span class="nav-link-text">Email Subscription</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Accounts">
+          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#Accounts" data-parent="#exampleAccordion">
+            <i class="fa fa-align-left" aria-hidden="true"></i>
+            <span class="nav-link-text">Accounts</span>
+          </a>
+          <ul class="sidenav-second-level collapse" id="Accounts">
+            <li>
+              <a href="navbar.html">All User</a>
+            </li>
+            <li>
+              <a href="cards.html">Suspended User</a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
+          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseUsers" data-parent="#exampleAccordion">
+            <i class="fa fa-users" aria-hidden="true"></i>
+            <span class="nav-link-text">Users</span>
+          </a>
+          <ul class="sidenav-second-level collapse" id="collapseUsers">
+            <li>
+              <a href="navbar.html">All User</a>
+            </li>
+            <li>
+              <a href="cards.html">Suspended User</a>
+            </li>
+          </ul>
+        </li>
+
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Product">
+          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#Product" data-parent="#exampleAccordion">
+            <i class="fa fa-align-left" aria-hidden="true"></i>
+            <span class="nav-link-text">All Product</span>
+          </a>
+          <ul class="sidenav-second-level collapse" id="Product">
+            <li>
+              <a href="login.html">All Product</a>
+            </li>
+            <li>
+              <a href="register.html">Add Product</a>
+            </li>
+          </ul>
+        </li>
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
           <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseExamplePages" data-parent="#exampleAccordion">
-            <i class="fa fa-fw fa-file"></i>
+          	<i class="fa fa-th-large" aria-hidden="true"></i>
             <span class="nav-link-text">Portfolio</span>
           </a>
           <ul class="sidenav-second-level collapse" id="collapseExamplePages">
@@ -86,7 +121,7 @@
         </li>
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
           <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti" data-parent="#exampleAccordion">
-            <i class="fa fa-fw fa-sitemap"></i>
+            <i class="fa fa-align-left" aria-hidden="true"></i>
             <span class="nav-link-text">Our Service</span>
           </a>
           <ul class="sidenav-second-level collapse" id="collapseMulti">
@@ -104,10 +139,16 @@
             </li>
           </ul>
         </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
-          <a class="nav-link" href="#">
-            <i class="fa fa-fw fa-link"></i>
-            <span class="nav-link-text">Link</span>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="msg">
+          <a class="nav-link" href="index.html">
+            <i class="fa fa-envelope-o" aria-hidden="true"></i>
+            <span class="nav-link-text">Message Board</span>
+          </a>
+        </li>
+        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Report">
+          <a class="nav-link" href="index.html">
+          	<i class="fa fa-line-chart" aria-hidden="true"></i>
+            <span class="nav-link-text">Report</span>
           </a>
         </li>
       </ul>
@@ -119,83 +160,7 @@
         </li>
       </ul>
       <ul class="navbar-nav ml-auto">
-        <!-- <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa fa-fw fa-envelope"></i>
-            <span class="d-lg-none">Messages
-              <span class="badge badge-pill badge-primary">12 New</span>
-            </span>
-            <span class="indicator text-primary d-none d-lg-block">
-              <i class="fa fa-fw fa-circle"></i>
-            </span>
-          </a>
-          <div class="dropdown-menu" aria-labelledby="messagesDropdown">
-            <h6 class="dropdown-header">New Messages:</h6>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <strong>David Miller</strong>
-              <span class="small float-right text-muted">11:21 AM</span>
-              <div class="dropdown-message small">Hey there! This new version of SB Admin is pretty awesome! These messages clip off when they reach the end of the box so they don't overflow over to the sides!</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <strong>Jane Smith</strong>
-              <span class="small float-right text-muted">11:21 AM</span>
-              <div class="dropdown-message small">I was wondering if you could meet for an appointment at 3:00 instead of 4:00. Thanks!</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <strong>John Doe</strong>
-              <span class="small float-right text-muted">11:21 AM</span>
-              <div class="dropdown-message small">I've sent the final files over to you for review. When you're able to sign off of them let me know and we can discuss distribution.</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item small" href="#">View all messages</a>
-          </div>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <i class="fa fa-fw fa-bell"></i>
-            <span class="d-lg-none">Alerts
-              <span class="badge badge-pill badge-warning">6 New</span>
-            </span>
-            <span class="indicator text-warning d-none d-lg-block">
-              <i class="fa fa-fw fa-circle"></i>
-            </span>
-          </a>
-          <div class="dropdown-menu" aria-labelledby="alertsDropdown">
-            <h6 class="dropdown-header">New Alerts:</h6>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <span class="text-success">
-                <strong>
-                  <i class="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
-              </span>
-              <span class="small float-right text-muted">11:21 AM</span>
-              <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <span class="text-danger">
-                <strong>
-                  <i class="fa fa-long-arrow-down fa-fw"></i>Status Update</strong>
-              </span>
-              <span class="small float-right text-muted">11:21 AM</span>
-              <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-              <span class="text-success">
-                <strong>
-                  <i class="fa fa-long-arrow-up fa-fw"></i>Status Update</strong>
-              </span>
-              <span class="small float-right text-muted">11:21 AM</span>
-              <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
-            </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item small" href="#">View all alerts</a>
-          </div>
-        </li>
+
         <li class="nav-item">
           <form class="form-inline my-2 my-lg-0 mr-lg-2">
             <div class="input-group">
@@ -207,7 +172,15 @@
               </span>
             </div>
           </form>
-        </li>-->
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="">
+            <i class="fa fa-fw fa-envelope"></i>
+            <span class="d-lg-none">Messages
+              <span class="badge badge-pill badge-primary">12 New</span>
+            </span>
+          </a>
+        </li>
         <li class="nav-item">
           <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
             <i class="fa fa-fw fa-sign-out"></i>Logout</a>
