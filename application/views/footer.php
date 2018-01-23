6cd29b25efb27facf73cc5d8a60f075332857f6b
AMIT ANAND <amitanand.kvj.2@gmail.com>
2018-01-23 20:13:30 +0530

    mmm

---
+++ application/views/footer.php
@@ -0,0 +1,352 @@
+		<!-- start Footer Wrapper -->
+		
+		<div class="footer-wrapper scrollspy-footer">
+		
+			<footer class="main-footer">
+			
+				<div class="container">
+				
+					<div class="row">
+					
+						<div class="col-sm-12 col-md-4">
+						
+							<h5 class="footer-title">newsletter</h5>
+							
+							<p class="font16">Subsribe to get our latest updates and oeffers</p>
+							
+							<div class="footer-newsletter">
+								
+								<div class="form-group">
+									<input class="form-control" placeholder="enter your email " />
+									<button class="btn btn-primary">subsribe</button>
+								</div>
+								
+								<p class="font-italic font13">*** Don't worry, we wont spam you!</p>
+							
+							</div>
+
+						</div>
+						
+						<div class="col-sm-12 col-md-8">
+						
+							<div class="row">
+								
+								<div class="col-xs-12 col-sm-4 col-md-3 col-md-offset-3 mt-25-sm">
+									<h5 class="footer-title">footer</h5>
+									<ul class="footer-menu">
+										<li><a href="#">Support</a></li>
+										<li><a href="#">Advertise</a></li>
+										<li><a href="#">Media Relations</a></li>
+										<li><a href="#">Affiliates</a></li>
+										<li><a href="#">Careers</a></li>
+									</ul>
+								</div>
+								
+								<div class="col-xs-12 col-sm-4 col-md-3 mt-25-sm">
+									<h5 class="footer-title">quick  links</h5>
+									<ul class="footer-menu">
+										<li><a href="#">Media Relations</a></li>
+										<li><a href="#">Affiliates</a></li>
+										<li><a href="#">Careers</a></li>
+										<li><a href="#">Support</a></li>
+										<li><a href="#">Advertise</a></li>
+									</ul>
+								</div>
+								
+								<div class="col-xs-12 col-sm-4 col-md-3 mt-25-sm">
+								
+									<h5 class="footer-title">helps</h5>
+									<ul class="footer-menu">
+										<li><a href="#">Using a Tour</a></li>
+										<li><a href="#">Submitting a Tour</a></li>
+										<li><a href="#">Managing My Account</a></li>
+										<li><a href="#">Merchant Help</a></li>
+										<li><a href="#">White Label Website</a></li>
+									</ul>
+								
+								</div>
+
+							</div>
+
+						</div>
+						
+					</div>
+					
+				</div>
+				
+			</footer>
+			
+			<footer class="bottom-footer">
+			
+				<div class="container">
+				
+					<div class="row">
+					
+						<div class="col-xs-12 col-sm-6 col-md-4">
+				
+							<p class="copy-right">&#169; 2017 Togoby - tour hosting</p>
+							
+						</div>
+						
+						<div class="col-xs-12 col-sm-6 col-md-4">
+						
+							<ul class="bottom-footer-menu">
+								<li><a href="#">Cookies</a></li>
+								<li><a href="#">Policies</a></li>
+								<li><a href="#">Terms</a></li>
+								<li><a href="#">Blogs</a></li>
+							</ul>
+						
+						</div>
+						
+						<div class="col-xs-12 col-sm-12 col-md-4">
+							<ul class="bottom-footer-menu for-social">
+								<li><a href="#"><i class="icon-social-twitter" data-toggle="tooltip" data-placement="top" title="twitter"></i></a></li>
+								<li><a href="#"><i class="icon-social-facebook" data-toggle="tooltip" data-placement="top" title="facebook"></i></a></li>
+								<li><a href="#"><i class="icon-social-google" data-toggle="tooltip" data-placement="top" title="google plus"></i></a></li>
+								<li><a href="#"><i class="icon-social-instagram" data-toggle="tooltip" data-placement="top" title="instrgram"></i></a></li>
+							</ul>
+						</div>
+					
+					</div>
+
+				</div>
+				
+			
+			</footer>
+			
+		</div>
+		
+		<!-- end Footer Wrapper -->
+		
+	</div> 
+	
+	<!-- end Container Wrapper -->
+ 
+ 
+<!-- start Back To Top -->
+
+<div id="back-to-top">
+   <a href="#"><i class="ion-ios-arrow-up"></i></a>
+</div>
+
+<!-- end Back To Top -->
+
+
+<!-- start Sign-in Modal -->
+<div id="loginModal" class="modal fade login-box-wrapper" tabindex="-1" data-width="550" data-backdrop="static" data-keyboard="false" data-replace="true">
+
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		<h4 class="modal-title text-center">Sign-in into your account</h4>
+	</div>
+	
+	<div class="modal-body">
+		<div class="row gap-20">
+		
+			<div class="col-sm-6 col-md-6">
+				<button class="btn btn-facebook btn-block mb-5-xs">Log-in with Facebook</button>
+			</div>
+			<div class="col-sm-6 col-md-6">
+				<button class="btn btn-google-plus btn-block">Log-in with Google+</button>
+			</div>
+			
+			<div class="col-md-12">
+				<div class="login-modal-or">
+					<div><span>or</span></div>
+				</div>
+			</div>
+			
+			<div class="col-sm-12 col-md-12">
+	
+				<div class="form-group"> 
+					<label>Username</label>
+					<input class="form-control" placeholder="Min 4 and Max 10 characters" type="text"> 
+				</div>
+			
+			</div>
+			
+			<div class="col-sm-12 col-md-12">
+			
+				<div class="form-group"> 
+					<label>Password</label>
+					<input class="form-control" placeholder="Min 4 and Max 10 characters" type="text"> 
+				</div>
+			
+			</div>
+			
+			<div class="col-sm-6 col-md-6">
+				<div class="checkbox-block"> 
+					<input id="remember_me_checkbox" name="remember_me_checkbox" class="checkbox" value="First Choice" type="checkbox"> 
+					<label class="" for="remember_me_checkbox">Remember me</label>
+				</div>
+			</div>
+			
+			<div class="col-sm-6 col-md-6">
+				<div class="login-box-link-action">
+					<a data-toggle="modal" href="#forgotPasswordModal" class="block line18 mt-1">Forgot password?</a> 
+				</div>
+			</div>
+			
+			<div class="col-sm-12 col-md-12">
+				<div class="login-box-box-action">
+					No account? <a data-toggle="modal" href="#registerModal">Register</a>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	
+	<div class="modal-footer text-center">
+		<button type="button" class="btn btn-primary">Log-in</button>
+		<button type="button" data-dismiss="modal" class="btn btn-primary btn-border">Close</button>
+	</div>
+	
+</div>
+<!-- end Sign-in Modal -->
+
+<!-- start Register Modal -->
+<div id="registerModal" class="modal fade login-box-wrapper" tabindex="-1" data-backdrop="static" data-keyboard="false" data-replace="true">
+
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		<h4 class="modal-title text-center">Create your account for free</h4>
+	</div>
+	
+	<div class="modal-body">
+	
+		<div class="row gap-20">
+		
+			<div class="col-sm-6 col-md-6">
+				<button class="btn btn-facebook btn-block mb-5-xs">Register with Facebook</button>
+			</div>
+			<div class="col-sm-6 col-md-6">
+				<button class="btn btn-google-plus btn-block">Register with Google+</button>
+			</div>
+			
+			<div class="col-md-12">
+				<div class="login-modal-or">
+					<div><span>or</span></div>
+				</div>
+			</div>
+			
+			<div class="col-sm-12 col-md-12">
+	
+				<div class="form-group"> 
+					<label>Username</label>
+					<input class="form-control" placeholder="Min 4 and Max 10 characters" type="text"> 
+				</div>
+			
+			</div>
+			
+			<div class="col-sm-12 col-md-12">
+	
+				<div class="form-group"> 
+					<label>Email Address</label>
+					<input class="form-control" placeholder="Enter your email address" type="text"> 
+				</div>
+			
+			</div>
+			
+			<div class="col-sm-12 col-md-12">
+			
+				<div class="form-group"> 
+					<label>Password</label>
+					<input class="form-control" placeholder="Min 8 and Max 20 characters" type="text"> 
+				</div>
+			
+			</div>
+			
+			<div class="col-sm-12 col-md-12">
+			
+				<div class="form-group"> 
+					<label>Password Confirmation</label>
+					<input class="form-control" placeholder="Re-type password again" type="text"> 
+				</div>
+			
+			</div>
+			
+			<div class="col-sm-12 col-md-12">
+				<div class="checkbox-block"> 
+					<input id="register_accept_checkbox" name="register_accept_checkbox" class="checkbox" value="First Choice" type="checkbox"> 
+					<label class="" for="register_accept_checkbox">By register, I read &amp; accept <a href="#">the terms</a></label>
+				</div>
+			</div>
+			
+			<div class="col-sm-12 col-md-12">
+				<div class="login-box-box-action">
+					Already have account? <a data-toggle="modal" href="#loginModal">Log-in</a>
+				</div>
+			</div>
+			
+		</div>
+	
+	</div>
+	
+	<div class="modal-footer text-center">
+		<button type="button" class="btn btn-primary">Register</button>
+		<button type="button" data-dismiss="modal" class="btn btn-primary btn-border">Close</button>
+	</div>
+	
+</div>
+<!-- end Register Modal -->
+
+<!-- start Forget Password Modal -->
+<div id="forgotPasswordModal" class="modal fade login-box-wrapper" tabindex="-1" data-backdrop="static" data-keyboard="false" data-replace="true">
+
+	<div class="modal-header">
+		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		<h4 class="modal-title text-center">Restore your forgotten password</h4>
+	</div>
+	
+	<div class="modal-body">
+		<div class="row gap-20">
+			
+			<div class="col-sm-12 col-md-12">
+				<p class="mb-20">Maids table how learn drift but purse stand yet set. Music me house could among oh as their. Piqued our sister shy nature almost his wicket. Hand dear so we hour to.</p>
+			</div>
+			
+			<div class="col-sm-12 col-md-12">
+	
+				<div class="form-group"> 
+					<label>Email Address</label>
+					<input class="form-control" placeholder="Enter your email address" type="text"> 
+				</div>
+			
+			</div>
+
+			<div class="col-sm-12 col-md-12">
+				<div class="checkbox-block"> 
+					<input id="forgot_password_checkbox" name="forgot_password_checkbox" class="checkbox" value="First Choice" type="checkbox"> 
+					<label class="" for="forgot_password_checkbox">Generate new password</label>
+				</div>
+			</div>
+			
+			<div class="col-sm-12 col-md-12">
+				<div class="login-box-box-action">
+					Return to <a data-toggle="modal" href="#loginModal">Log-in</a>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	
+	<div class="modal-footer text-center">
+		<button type="button" class="btn btn-primary">Restore</button>
+		<button type="button" data-dismiss="modal" class="btn btn-primary btn-border">Close</button>
+	</div>
+	
+</div>
+<!-- end Forget Password Modal -->
+ 
+<!-- Core JS -->
+<script type="text/javascript" src="<?php echo base_url();?>media/js/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>media/js/core-plugins.js"></script>
+<script type="text/javascript" src="<?php echo base_url();?>media/js/customs.js"></script>
+
+<!-- Only in Home Page -->
+<script type="text/javascript" src="<?php echo base_url();?>media/js/jquery.flexdatalist.js"></script>
+
+</body>
+
+</html>
---
+++ application/views/header.php
@@ -0,0 +1,215 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- Title Of Site -->
+	<title>Togoby - Tour hosting</title>
+	<meta name="description" content="HTML template for multiple tour agency, local agency, traveller, tour hosting based on Twitter Bootstrap 3.x.x" />
+	<meta name="keywords" content="tour agency, tour guide, travel, trip, holiday, vocation, relax, adventure, virtual tour, tour planner" />
+	<meta name="author" content="crenoveative">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	
+	<!-- Fav and Touch Icons -->
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo base_url();?>/media/images/ico/apple-touch-icon-144-precomposed.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo base_url();?>/media/images/ico/apple-touch-icon-114-precomposed.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo base_url();?>/media/images/ico/apple-touch-icon-72-precomposed.png">
+	<link rel="apple-touch-icon" href="<?php echo base_url();?>/media/images/ico/apple-touch-icon-57-precomposed.png">
+	<link rel="shortcut icon" href="<?php echo base_url();?>/media/images/ico/favicon.png">
+
+	<!-- CSS Plugins -->
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>/media/bootstrap/css/bootstrap.min.css" media="screen">	
+	<link href="<?php echo base_url();?>/media/css/main.css" rel="stylesheet">
+	<link href="<?php echo base_url();?>/media/css/plugin.css" rel="stylesheet">
+
+	<!-- CSS Custom -->
+	<link href="<?php echo base_url();?>/media/css/style.css" rel="stylesheet">
+	
+	<!-- Add your style -->
+	<link href="<?php echo base_url();?>/media/css/your-style.css" rel="stylesheet">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+	
+</head>
+
+<body class="home transparent-header">
+
+	<div id="introLoader" class="introLoading"></div>
+
+	<!-- start Container Wrapper -->
+	
+	<div class="container-wrapper">
+
+		<!-- start Header -->
+		
+		<header id="header">
+
+			<!-- start Navbar (Header) -->
+			<nav class="navbar navbar-default navbar-fixed-top navbar-sticky-function navbar-arrow">
+
+				<div class="container">
+					
+					<div class="logo-wrapper">
+						<div class="logo">
+							<a href="index.html"><img src="images/logo-white.png" alt="Logo" /></a>
+						</div>
+					</div>
+					
+					<div id="navbar" class="navbar-nav-wrapper">
+					
+						<ul class="nav navbar-nav" id="responsive-menu">
+						
+							<li>
+								<a href="index.html">Home</a>
+								<ul>
+									<li><a href="index-02.html">Home 02</a></li>
+									<li><a href="index-03.html">Home 03</a></li>
+									<li><a href="index-04.html">Home 04</a></li>
+									<li><a href="index-05.html">Home 05</a></li>
+									<li><a href="index-06.html">Home 06</a></li>
+									<li><a href="index-07.html">Home 07</a></li>
+									<li><a href="index-08.html">Home 08</a></li>
+									<li><a href="index-09.html">Home 09</a></li>
+									<li><a href="index-10.html">Home 10</a></li>
+									<li><a href="index-11.html">Home 11</a></li>
+									<li><a href="index-12.html">Home 12</a></li>
+								</ul>
+							</li>
+							
+							<li>
+								<a href="offered.html">Offered Tour</a>
+								<ul>
+									<li>
+										<a href="offered-result.html">Offered Tour Result</a>
+										<ul>
+											<li><a href="offered-result-02.html">Offered Tour Result 02</a></li>
+										</ul>
+									</li>
+									<li>
+										<a href="offered-detail.html">Offered Tour Detail</a>
+										<ul>
+											<li><a href="offered-detail-02.html">Offered Tour Detail 02</a></li>
+											<li><a href="offered-detail-03.html">Offered Tour Detail 03</a></li>
+											<li><a href="offered-detail-04.html">Offered Tour Detail 04</a></li>
+											<li><a href="offered-detail-05.html">Offered Tour Detail 05</a></li>
+											<li><a href="offered-detail-06.html">Offered Tour Detail 06</a></li>
+											<li><a href="offered-detail-07.html">Offered Tour Detail 07</a></li>
+											<li><a href="offered-detail-08.html">Offered Tour Detail 08</a></li>
+										</ul>
+									</li>
+									<li><a href="offered-payment.html">Offered Detail Payment</a></li>
+									<li><a href="offered-payment-done.html">Offered Detail Payment Done</a></li>
+									<li><a href="offered-create.html">Offered Detail Create</a></li>
+									<li><a href="offered-create-done.html">Offered Detail Create Done</a></li>
+								</ul>
+							</li>
+							
+							<li>
+								<a href="requested.html">Requested Tour</a>
+								<ul>
+									<li>
+										<a href="requested-result.html">Requested Tour Result</a>
+										<ul>
+											<li><a href="requested-result-02.html">Requested Tour Result 02</a></li>
+										</ul>
+									</li>
+									<li><a href="requested-detail.html">Requested Tour Detail</a></li>
+									<li><a href="requested-requested-bid.html">Requested Tour Bid</a></li>
+									<li><a href="requested-requested-bid-open.html">Requested Tour Bid Open</a></li>
+									<li><a href="requested-create.html">Requested Tour Create</a></li>
+									<li><a href="requested-create-done.html">Requested Tour Create Done</a></li>
+									<li><a href="requested-detail-travel-history.html">Requested Travel History</a></li>
+								</ul>
+							</li>
+							
+							<li>
+								<a href="guide.html">Guide</a>
+								<ul>
+									<li>
+										<a href="guide-result.html">Guide Result</a>
+										<ul>
+											<li><a href="guide-result-02.html">Guide Result 02</a></li>
+										</ul>
+									</li>
+									<li><a href="guide-detail.html">Guide Detail</a></li>
+									<li><a href="guide-payment.html">Guide Payment</a></li>
+									<li><a href="guide-payment-done.html">Guide Payment Done</a></li>
+									<li><a href="guide-detail-offer.html">Guide - Tour</a></li>
+									<li><a href="guide-detail-gallery.html">Guide - Gallery</a></li>
+									<li><a href="guide-detail-reviews.html">Guide - Review</a></li>
+									<li><a href="guide-detail-follower.html">Guide - Follower</a></li>
+								</ul>
+							</li>
+							
+							<li>
+								<a href="#">Pages</a>
+								<ul>
+									<li>
+										<a href="guide-detail-setting.html">Dashboard</a>
+										<ul>
+											<li><a href="guide-detail-setting-edit-profile.html">Dashboard - Edit Profile</a></li>
+											<li><a href="guide-detail-setting-guide-information.html">Dashboard - Personal Information</a></li>
+											<li><a href="guide-detail-setting-change-pass.html">Dashboard - Change Password</a></li>
+											<li><a href="guide-detail-setting-my-tour.html">Dashboard - My Tour</a></li>
+											<li><a href="guide-detail-setting-my-wihslist.html">Dashboard - My Wishlist</a></li>
+										</ul>
+									</li>
+									<li><a href="about-us.html">About Us</a></li>
+									<li><a href="contact-us.html">Contact Us</a></li>
+									<li>
+										<a href="faq.html">FAQ</a>
+										<ul>
+											<li><a href="faq-02.html">FAQ 2</a></li>
+										</ul>
+									</li>
+									<li>
+										<a href="blog.html">Blog</a>
+										<ul>
+											<li><a href="blog-single.html">Blog 2</a></li>
+										</ul>
+									</li>
+									<li><a href="404-error-page.html">404 - Error Page</a></li>
+									<li><a href="static-page.html">Static Page</a></li>
+									<li>
+										<a href="#">Shortcode</a>
+										<ul>
+											<li><a href="shortcode-typography.html">Shortcode - Typography</a></li>
+											<li><a href="shortcode-element.html">Shortcode - Element</a></li>
+											<li><a href="shortcode-component.html">Shortcode - Component</a></li>
+											<li><a href="shortcode-layout-fullwidth.html">Shortcode - Layout Full Width</a></li>
+											<li><a href="shortcode-layout-left-sidebar.html">Shortcode - Layout Left Sidebar</a></li>
+											<li><a href="shortcode-layout-right-sidebar.html">Shortcode - Layout Right Sidebar</a></li>
+										</ul>
+									</li>
+								</ul>
+							</li>
+							
+						</ul>
+				
+					</div><!--/.nav-collapse -->
+
+					<div class="nav-mini-wrapper">
+						<ul class="nav-mini">
+							<li><a data-toggle="modal" href="#registerModal"><i class="icon-user-follow" data-toggle="tooltip" data-placement="bottom" title="sign up"></i></a></li>
+							<li><a data-toggle="modal" href="#loginModal"><i class="icon-login" data-toggle="tooltip" data-placement="bottom" title="login"></i> </a></li>
+						</ul>
+					</div>
+				
+				</div>
+				
+				<div id="slicknav-mobile"></div>
+				
+			</nav>
+			<!-- end Navbar (Header) -->
+
+		</header>
+		
+		<!-- end Header -->
