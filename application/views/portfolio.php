c23e88f48cce6a8dccb93448feff3a6b94226f63
rrptm <raviranjan.ptm@gmail.com>
2018-01-23 21:40:14 +0530

    portfolio

---
+++ application/views/portfolio.php
@@ -0,0 +1,113 @@
+<div class="container pt-70 pb-60 clearfix">
+
+				<div class="row">
+				
+					<div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
+					
+						<div class="section-title">
+						
+							<h2>Portfolio</h2>
+							
+						</div>
+					
+					</div>
+				
+				</div>
+				
+				<div class="mb-30">
+				
+					<div class="GridLex-gap-20 GridLex-gap-15-mdd GridLex-gap-10-xs">
+						
+						<div class="GridLex-grid-noGutter-equalHeight">
+						
+							<div class="GridLex-col-4_sm-4_xs-6_xss-12">
+							
+								<div class="top-destination-item">
+									<a href="#">
+										<div class="image">
+											<img src="<?php echo base_url();?>/media/images/top-destinations/01.jpg" alt="images" />
+										</div>
+										<h4 class="uppercase"><span>Italy</span></h4>
+										<p>254 packages<br /> 142 requests<br /> 52 guides</p>
+									</a>
+								</div>
+								
+							</div>
+							
+							<div class="GridLex-col-4_sm-4_xs-6_xss-12">
+							
+								<div class="top-destination-item">
+									<a href="#">
+										<div class="image">
+											<img src="<?php echo base_url();?>/media/images/top-destinations/02.jpg" alt="images" />
+										</div>
+										<h4 class="uppercase"><span>USA</span></h4>
+										<p>254 packages<br /> 142 requests<br /> 52 guides</p>
+									</a>
+								</div>
+								
+							</div>
+							
+							<div class="GridLex-col-4_sm-4_xs-6_xss-12">
+							
+								<div class="top-destination-item">
+									<a href="#">
+										<div class="image">
+											<img src="<?php echo base_url();?>/media/images/top-destinations/03.jpg" alt="images" />
+										</div>
+										<h4 class="uppercase"><span>Emerate</span></h4>
+										<p>254 packages<br /> 142 requests<br /> 52 guides</p>
+									</a>
+								</div>
+								
+							</div>
+							
+							<div class="GridLex-col-4_sm-4_xs-6_xss-12">
+							
+								<div class="top-destination-item">
+									<a href="#">
+										<div class="image">
+											<img src="<?php echo base_url();?>/media/images/top-destinations/04.jpg" alt="images" />
+										</div>
+										<h4 class="uppercase"><span>Greece</span></h4>
+										<p>254 packages<br /> 142 requests<br /> 52 guides</p>
+									</a>
+								</div>
+								
+							</div>
+							
+							<div class="GridLex-col-4_sm-4_xs-6_xss-12">
+							
+								<div class="top-destination-item">
+									<a href="#">
+										<div class="image">
+											<img src="<?php echo base_url();?>/media/images/top-destinations/05.jpg" alt="images" />
+										</div>
+										<h4 class="uppercase"><span>Maldives</span></h4>
+										<p>254 packages<br /> 142 requests<br /> 52 guides</p>
+									</a>
+								</div>
+								
+							</div>
+							
+							<div class="GridLex-col-4_sm-4_xs-6_xss-12">
+							
+								<div class="top-destination-item">
+									<a href="#">
+										<div class="image">
+											<img src="<?php echo base_url();?>/media/images/top-destinations/06.jpg" alt="images" />
+										</div>
+										<h4 class="uppercase"><span>Australia</span></h4>
+										<p>254 packages<br /> 142 requests<br /> 52 guides</p>
+									</a>
+								</div>
+								
+							</div>
+							
+						</div>
+						
+					</div>
+
+				</div>
+				
+			</div>
