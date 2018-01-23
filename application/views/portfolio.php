cb443bc7d306dd6123af4d2b12698e9dcabec7a6
rrptm <raviranjan.ptm@gmail.com>
2018-01-23 22:46:48 +0530

    c

--- application/views/portfolio.php
+++ application/views/portfolio.php
@@ -1,3 +1,9 @@
+
+			
+			
+
+<!--portfolio-->
+
 <div class="container pt-70 pb-60 clearfix">
 
 				<div class="row">
@@ -13,6 +19,102 @@
 					</div>
 				
 				</div>
+
+
+		<!-- filter -->
+			
+			<div class="filter-full-width-wrapper">
+
+				<form class="">
+				
+					<div class="filter-full-primary">
+					
+						<div class="container">
+					
+							<div class="filter-full-secondary-inner">
+							
+								<div class="form-holder">
+								
+									<div class="row">
+									
+										<div class="col-xs-12 col-sm-12 col-md-6">
+										
+											<div class="filter-item bb-sm no-bb-xss">
+											
+												<div class="input-group input-group-addon-icon no-border no-br-sm">
+													<span class="input-group-addon input-group-addon-icon bg-white"><label><i class="fa fa-map-marker"></i> Destination:</label></span>
+													<input type="text" class="form-control" id="autocompleteTagging" value="Paris, Rome, Bangkok" placeholder="" />
+												</div>
+											
+											</div>
+											
+										</div>
+
+										<div class="col-xs-12 col-sm-12 col-md-6">
+										
+											<div class="filter-item-wrapper">
+											
+												<div class="row">
+													
+													<div class="col-xss-12 col-xs-6 col-sm-6">
+											
+														<div class="filter-item mmr">
+														
+															<div class="input-group input-group-addon-icon no-border no-br-xs">
+																<span class="input-group-addon input-group-addon-icon bg-white">
+																<label class="block-xs"><i class="fa fa-sort-amount-asc"></i> Sort by:</label></span>
+																<select class="selectpicker form-control block-xs">
+																	<option value="0"> Price</option>
+																	<option value="3"> Name</option>
+																	<option value="4"> User Rating</option>
+																	<option value="7"> Star Rating</option>
+																</select>
+															</div>
+														
+														</div>
+														
+													</div>
+											
+													<div class="col-xss-12 col-xs-6 col-sm-5">
+													
+														<div class="filter-item mmr">
+														
+															<div class="input-group input-group-addon-icon no-border no-br-xs">
+																<span class="input-group-addon input-group-addon-icon bg-white"><label><i class="fa fa-sort-amount-asc"></i> Trip Style:</label></span>
+																<select class="selectpicker form-control" data-live-search="false" data-selected-text-format="count > 2" data-done-button="true" data-done-button-text="OK" data-none-selected-text="All Types" multiple>
+																	<option value="0"> Adventure</option>
+																	<option value="1"> Hooneymoon</option>
+																	<option value="2"> Shopping</option>
+																	<option value="3"> History</option>
+																</select>
+															</div>
+														
+														</div>
+														
+													</div>
+												
+												</div>
+											
+											</div>
+											
+										</div>
+
+									</div>
+								
+								</div>
+								
+								
+							</div>
+
+						</div>
+
+					</div></div><br>
+				<!-- filter end -->	
+
+
+
+
+
 				
 				<div class="mb-30">
 				
@@ -20,91 +122,135 @@
 						
 						<div class="GridLex-grid-noGutter-equalHeight">
 						
-							<div class="GridLex-col-4_sm-4_xs-6_xss-12">
+							<div class="GridLex-col-3_sm-3_xs-6_xss-12">
 							
 								<div class="top-destination-item">
 									<a href="#">
 										<div class="image">
 											<img src="<?php echo base_url();?>/media/images/top-destinations/01.jpg" alt="images" />
 										</div>
-										<h4 class="uppercase"><span>Category</span></h4>
-										<p style="font-size: 30px;">Name </p>
+										
 									</a>
 								</div>
 								
 							</div>
 							
-							<div class="GridLex-col-4_sm-4_xs-6_xss-12">
+							<div class="GridLex-col-3_sm-3_xs-6_xss-12">
 							
 								<div class="top-destination-item">
 									<a href="#">
 										<div class="image">
 											<img src="<?php echo base_url();?>/media/images/top-destinations/02.jpg" alt="images" />
 										</div>
-										<h4 class="uppercase"><span>Category</span></h4>
-										<p style="font-size: 30px;">Name </p>
+										
 									</a>
 								</div>
 								
 							</div>
 							
-							<div class="GridLex-col-4_sm-4_xs-6_xss-12">
+							<div class="GridLex-col-3_sm-3_xs-6_xss-12">
 							
 								<div class="top-destination-item">
 									<a href="#">
 										<div class="image">
 											<img src="<?php echo base_url();?>/media/images/top-destinations/03.jpg" alt="images" />
 										</div>
-										<h4 class="uppercase"><span>Category</span></h4>
-										<p style="font-size: 30px;">Name </p>
+										
 									</a>
 								</div>
 								
 							</div>
 							
-							<div class="GridLex-col-4_sm-4_xs-6_xss-12">
+							<div class="GridLex-col-3_sm-3_xs-6_xss-12">
 							
 								<div class="top-destination-item">
 									<a href="#">
 										<div class="image">
 											<img src="<?php echo base_url();?>/media/images/top-destinations/04.jpg" alt="images" />
 										</div>
-										<h4 class="uppercase"><span>Category</span></h4>
-										<p style="font-size: 30px;">Name </p>
+										
 									</a>
 								</div>
 								
 							</div>
 							
-							<div class="GridLex-col-4_sm-4_xs-6_xss-12">
+							<div class="GridLex-col-3_sm-3_xs-6_xss-12">
 							
 								<div class="top-destination-item">
 									<a href="#">
 										<div class="image">
 											<img src="<?php echo base_url();?>/media/images/top-destinations/05.jpg" alt="images" />
 										</div>
-										<h4 class="uppercase"><span>Category</span></h4>
-										<p style="font-size: 30px;">Name </p>
+										
+									</a>
+								</div>
+								
+							</div>
+							<div class="GridLex-col-3_sm-3_xs-6_xss-12">
+							
+								<div class="top-destination-item">
+									<a href="#">
+										<div class="image">
+											<img src="<?php echo base_url();?>/media/images/top-destinations/02.jpg" alt="images" />
+										</div>
+										
 									</a>
 								</div>
 								
 							</div>
 							
-							<div class="GridLex-col-4_sm-4_xs-6_xss-12">
+							<div class="GridLex-col-3_sm-3_xs-6_xss-12">
+							
+								<div class="top-destination-item">
+									<a href="#">
+										<div class="image">
+											<img src="<?php echo base_url();?>/media/images/top-destinations/03.jpg" alt="images" />
+										</div>
+										
+									</a>
+								</div>
+								
+							</div><div class="GridLex-col-3_sm-3_xs-6_xss-12">
+							
+								<div class="top-destination-item">
+									<a href="#">
+										<div class="image">
+											<img src="<?php echo base_url();?>/media/images/top-destinations/02.jpg" alt="images" />
+										</div>
+										
+									</a>
+								</div>
+								
+							</div>
+							
+							<div class="GridLex-col-3_sm-3_xs-6_xss-12">
+							
+								<div class="top-destination-item">
+									<a href="#">
+										<div class="image">
+											<img src="<?php echo base_url();?>/media/images/top-destinations/03.jpg" alt="images" />
+										</div>
+										
+									</a>
+								</div>
+								
+							</div>
+							
+							<div class="GridLex-col-3_sm-3_xs-6_xss-12">
 							
 								<div class="top-destination-item">
 									<a href="#">
 										<div class="image">
 											<img src="<?php echo base_url();?>/media/images/top-destinations/06.jpg" alt="images" />
 										</div>
-										<h4 class="uppercase"><span>Category</span></h4>
-										<p style="font-size: 30px;">Name </p>
+										
 									</a>
 								</div>
 								
 							</div>
 							
 						</div>
+
 						
 					</div>
 
