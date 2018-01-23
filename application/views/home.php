508fd9beae5e890078daf66719517f932d6f5c53
rrptm <raviranjan.ptm@gmail.com>
2018-01-24 03:08:58 +0530

    home

--- application/views/home.php
+++ application/views/home.php
@@ -17,57 +17,144 @@
 		</div>	
 	</div>
 	<!-- end hero-header -->
-	<div class="gap"></div>
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12">
+	<div class="gap" style="background-color: #fff;"></div>
+	<div class="container-fluid" >
+		<div class="row"  >
+			<div class="col-md-12" style="background-color: #fff;">
 				<div class="col-md-2">
-					<img class="img-responsive center-block serv" src="<?php echo base_url(); ?>media/images/services/des.png">
-					<h3 class="text-center servtxt">graphics design</h3>
+					<img class="img-responsive center-block serv" src="<?php echo base_url(); ?>media/images/services/gd.jpg">
+					
 				</div>
 				<div class="col-md-2">
-					<img class="img-responsive center-block serv" src="<?php echo base_url(); ?>media/images/services/dev.png">
-					<h3 class="text-center servtxt">Website Development</h3>
+					<img class="img-responsive center-block serv" src="<?php echo base_url(); ?>media/images/services/wd.jpg">
+					
 				</div>
 				<div class="col-md-2">
-					<img class="img-responsive center-block serv" src="<?php echo base_url(); ?>media/images/services/android.png">
-					<h3 class="text-center servtxt">Android Apps</h3>
+					<img class="img-responsive center-block serv" src="<?php echo base_url(); ?>media/images/services/aa.png">
+					
 				</div>
 				<div class="col-md-2">
-					<img class="img-responsive center-block serv" src="<?php echo base_url(); ?>media/images/services/marketing.png">
-					<h3 class="text-center servtxt">Digital Marketing</h3>
+					<img class="img-responsive center-block serv" src="<?php echo base_url(); ?>media/images/services/dm.png">
+					
 				</div>
 				<div class="col-md-2">
-					<img class="img-responsive center-block serv" src="<?php echo base_url(); ?>media/images/services/mmarketing.png">
-					<h3 class="text-center servtxt">Mobile Marketing</h3>
+					<img class="img-responsive center-block serv" src="<?php echo base_url(); ?>media/images/services/mm.png">
+					
 				</div>
 				<div class="col-md-2">
-					<img class="img-responsive center-block serv" src="<?php echo base_url(); ?>media/images/services/security.png">
-					<h3 class="text-center servtxt">Web Security</h3>
+					<img class="img-responsive center-block serv" src="<?php echo base_url(); ?>media/images/services/ws.png">
+					
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class="gap"></div>
-	<hr>
-	<div class="gap"></div>
+	<div class="gap" style="background-color: #fff;"></div>
+	
+
+
+
+	<div class="gap" ></div>
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-md-12">
-				<div class="col-md-4">
-					<iframe width="640" height="360" src="https://www.youtube.com/embed/IzYbQVNc6SU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+				<div class="col-md-6" style="padding-top: 70px;">
+					<iframe width="640" height="360"  src="https://www.youtube.com/embed/IzYbQVNc6SU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 				</div>
-				<div class="col-md-8 text-center">
-					<h1>What’s happening at 99workers?</h1>
+				<div class="col-md-6 text-center">
+					<h5>What’s happening at 99workers?</h5>
 					<p>Check out our blog to find out »</p>
-				</div>
+				
+			<div class="col-xs-12 col-sm-6 col-md-12 mt-20" style="padding-left: 70px;">
+							
+							
+							<div class="gallery-wrapper">
+							
+								<div class="GridLex-gap-5">
+															
+									<div class="GridLex-grid-noGutter-equalHeight GridLex-grid-middle">
+											
+										
+										
+										<div class="GridLex-col-3_sm-4_xs-4_xss-6">
+											<div class="sumi-gallery-wrapper clearfix">
+												<div class="gallery-item image-object-fit image-object-fit-cover">
+													<img data-sgallery="guideGallery" title="Guide Gallery" data-full="images/guide-lightbox/04.jpg" data-thumb="images/guide-lightbox/04.jpg" src="<?php echo base_url(); ?>media/images/guide-lightbox/04.jpg" alt="images" >
+												</div>
+											</div>
+										</div>
+										
+										<div class="GridLex-col-3_sm-4_xs-4_xss-6">
+											<div class="sumi-gallery-wrapper clearfix">
+												<div class="gallery-item image-object-fit image-object-fit-cover">
+													<img data-sgallery="guideGallery" title="Guide Gallery" data-full="images/guide-lightbox/05.jpg" data-thumb="images/guide-lightbox/05.jpg" src="<?php echo base_url(); ?>media/<?php echo base_url(); ?>media/images/guide-lightbox//05.jpg" alt="images" >
+												</div>
+											</div>
+										</div>
+										
+										<div class="GridLex-col-3_sm-4_xs-4_xss-6">
+											<div class="sumi-gallery-wrapper clearfix">
+												<div class="gallery-item image-object-fit image-object-fit-cover">
+													<img data-sgallery="guideGallery" title="Guide Gallery" data-full="images/guide-lightbox/06.jpg" data-thumb="images/guide-lightbox/06.jpg" src="<?php echo base_url(); ?>media/<?php echo base_url(); ?>media/images/guide-lightbox//06.jpg" alt="images" >
+												</div>
+											</div>
+										</div>
+										
+										<div class="GridLex-col-3_sm-4_xs-4_xss-6">
+											<div class="sumi-gallery-wrapper clearfix">
+												<div class="gallery-item image-object-fit image-object-fit-cover">
+													<img data-sgallery="guideGallery" title="Guide Gallery" data-full="images/guide-lightbox/07.jpg" data-thumb="images/guide-lightbox/07.jpg" src="<?php echo base_url(); ?>media/images/guide-lightbox/07.jpg" alt="images" >
+												</div>
+											</div>
+										</div>
+										
+										<div class="GridLex-col-3_sm-4_xs-4_xss-6">
+											<div class="sumi-gallery-wrapper clearfix">
+												<div class="gallery-item image-object-fit image-object-fit-cover">
+													<img data-sgallery="guideGallery" title="Guide Gallery" data-full="images/guide-lightbox/08.jpg" data-thumb="images/guide-lightbox/08.jpg" src="<?php echo base_url(); ?>media/images/guide-lightbox/08.jpg" alt="images" >
+												</div>
+											</div>
+										</div>
+										
+										<div class="GridLex-col-3_sm-4_xs-4_xss-6">
+											<div class="sumi-gallery-wrapper clearfix">
+												<div class="gallery-item image-object-fit image-object-fit-cover">
+													<img data-sgallery="guideGallery" title="Guide Gallery" data-full="images/guide-lightbox/09.jpg" data-thumb="images/guide-lightbox/09.jpg" src="<?php echo base_url(); ?>media/images/guide-lightbox/09.jpg" alt="images" >
+												</div>
+											</div>
+										</div>
+										
+										<div class="GridLex-col-3_sm-4_xs-4_xss-6">
+											<div class="sumi-gallery-wrapper clearfix">
+												<div class="gallery-item image-object-fit image-object-fit-cover">
+													<img data-sgallery="guideGallery" title="Guide Gallery" data-full="images/guide-lightbox/10.jpg" data-thumb="images/guide-lightbox/10.jpg" src="<?php echo base_url(); ?>media/images/guide-lightbox/10.jpg" alt="images" >
+												</div>
+											</div>
+										</div>
+										
+										<div class="GridLex-col-3_sm-4_xs-4_xss-6">
+											<div class="sumi-gallery-wrapper clearfix">
+												<div class="gallery-item image-object-fit image-object-fit-cover">
+													<img data-sgallery="guideGallery" title="Guide Gallery" data-full="images/guide-lightbox/11.jpg" data-thumb="images/guide-lightbox/11.jpg" src="<?php echo base_url(); ?>media/images/guide-lightbox/11.jpg" alt="images" >
+												</div>
+											</div>
+										</div>
+										
+									</div>
+									
+								</div></div>
+								
+							</div>
+							
+						</div>
+
+
 			</div>
 		</div>
 	</div>
 	<div class="gap"></div>
-	<div class="hero img-bg-01">
-		<div class="container">
-			<h1>We’re here to help you get your site online!</h1>
+	<div class="">
+		<div class="container-fluid text-center" style="background-color: #fff;"><br>
+			<h4>We’re here to help you get your site online!</h4>
 			<p>Yep, these folks right here. Here’s some tips to help you affordable graphics design and website development solutions.</p>
 			<div class="gap"></div>
 			<div class="row">
@@ -78,6 +165,11 @@
 					<a href="#" class="btn btn-primary btn-lg">Brows Our Portfolio</a>
 				</div>
 			</div>
+			<br><br>
+			<div>
+				<img src="<?php echo base_url(); ?>media/images/group7.jpg">
+			</div>
+			<br>
 		</div>	
 	</div>
 </div>
