a5ff15ae50bc169c5289ca4f2dcac16561f71993
rrptm <raviranjan.ptm@gmail.com>
2018-01-24 19:53:22 +0530

    services

---
+++ application/views/services.php
@@ -0,0 +1,54 @@
+<div class="container">
+<div class="row">
+	<div class="col-xs-12 col-sm-10 col-md-offset-1  col-md-10">
+		<center><h1 >Services</h1></center><br>
+
+		<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">			   
+	    <div class="hovereffect">
+	        <img class="img-responsive" src="http://placehold.it/350x200" alt="">
+	        <div class="overlay">
+	           <h2>Hover effect 1</h2>
+	           <a class="info" href="#">link here</a>
+	        </div><br>
+	    </div></div>
+	    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+	    <div class="hovereffect">
+	        <img class="img-responsive" src="http://placehold.it/350x200" alt="">
+	        <div class="overlay">
+	           <h2>Hover effect 1</h2>
+	           <a class="info" href="#">link here</a>
+	        </div><br>
+	    </div></div>
+	    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+	    <div class="hovereffect">
+	        <img class="img-responsive" src="http://placehold.it/350x200" alt="">
+	        <div class="overlay">
+	           <h2>Hover effect 1</h2>
+	           <a class="info" href="#">link here</a>
+	        </div><br>
+	    </div></div>
+	    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+	    <div class="hovereffect"  >
+	        <img class="img-responsive" src="http://placehold.it/350x200" alt="" ; >
+	        <div class="overlay">
+	           <h2>Hover effect 1</h2>
+	           <a class="info" href="#">link here</a>
+	        </div><br>
+	    </div></div>
+	    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+	    <div class="hovereffect">
+	        <img class="img-responsive" src="http://placehold.it/350x200" alt="">
+	        <div class="overlay">
+	           <h2>Hover effect 1</h2>
+	           <a class="info" href="#">link here</a>
+	        </div><br>
+	    </div></div>
+	</div>
+				
+</div>
+</div>
+<br>
+
+
+
+
