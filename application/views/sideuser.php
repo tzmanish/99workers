7a53eb24e29c6718f9cbb8a0d2cbcd9eacf7457d
AMIT ANAND <amitanand.kvj.2@gmail.com>
2018-01-26 16:41:20 +0530

    fsda
    sfd

---
+++ application/views/sideuser.php
@@ -0,0 +1,53 @@
+
+		
+		<div class="main-wrapper scrollspy-container">
+		
+			<!-- start Breadcrumb -->
+			
+			<div class="breadcrumb-wrapper">
+				
+			</div>
+			
+			
+
+			<div class="pt-30 pb-50">
+			
+				<div class="container">
+
+					<div class="row">
+						
+						<div class="col-xs-12 col-sm-4 col-md-3 mt-20">
+
+							<aside class="sidebar-wrapper pr-15 pr-0-xs">
+	
+								<div class="common-menu-wrapper">
+							
+									<ul class="common-menu-list">
+										
+										<li class="active"><a href="userdash"><span class=""><i class="glyphicon glyphicon-blackboard"></i>Dashboard</span></a></li>
+										<li><a href="#">Project</a></li>
+										<li><a href="#">All Project</a></li>
+										<li><a href="#">Completed Project</a></li>
+										<li><a href="#">Project Processing</a></li>
+										<li><a href="#">Post a Project</a></li>
+										
+										<li><a href="#" ><span class=""><i class="glyphicon glyphicon-th-large"></i>Account</span></a></li>
+
+										<li><a href="#">Transaction Report</a></li>
+										<li><a href="#">Payment Due</a></li>
+										<li><a href="editprofile"><span class=""><i class="glyphicon glyphicon-user"></i>Edit profile</span></a></li>
+										<li><a href="#"><span class=""><i class="glyphicon glyphicon-cog"></i>Settings</span></a></li>
+										<li><a href="#">General Settings</a></li>
+										<li><a href="changepass">Change password</a></li>
+										<li><a href="changemail"><span class=""><i class="glyphicon glyphicon-envelope"></i>Change Email</span></a></li>
+										<li><a href="#">Report</a></li>
+										<li><a href="#"><span class=""><i class="glyphicon glyphicon-log-out"></i>Logout</span></a></li>
+
+										
+									</ul>
+									
+								</div>
+								
+							</aside>
+						
+						</div>
