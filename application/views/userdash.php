a42ce7ae0ff93676ddd99421c8798ab472548a06
AMIT ANAND <amitanand.kvj.2@gmail.com>
2018-01-27 14:37:13 +0530

    sdfd
    s

--- application/views/userdash.php
+++ application/views/userdash.php
@@ -5,7 +5,6 @@
 							<div class="dashboard-wrapper">
 							
 								<h4 class="section-title">Hello <?php echo $this->session->userdata('fname'); echo " "; echo $this->session->userdata('lname');?></h4>
-								<p class="mmt-15 mb-20">Your last loged-in: <span class="text-primary">11:42:21 am - March 15, 2016</span></p>
 								
 								
 								
