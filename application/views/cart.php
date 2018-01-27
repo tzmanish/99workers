f864f09d67087993c64e4119e6ff5fc0f908cf9a
manish <tzmanish97@gmail.com>
2018-01-27 14:42:47 +0530

    fgh

--- application/views/cart.php
+++ application/views/cart.php
@@ -1,36 +1,47 @@
-<div class="spacer"></div>
-<div id="startchange"></div>
-<div class="page-header ">
-		<h1>My Cart</h1>	
-</div>
 <div class="container">
+      <h1>My Cart</h1>  
         <div class="col-xs-12 col-md-9" >
+            <?php 
+                if ($cart = $this->cart->contents()){$i=0;
+                    foreach ($cart as $item ) {
+              $details=$this->user->showproduct1($item['id']);
+             ?>
+            <div class="col-xs-12 col-md-12  cart-layout cart_item" id="cart_<?php echo $item['id'];?>">
+                <div class="col-md-1 col-xs-12 cart-line text-right">
+                    <h5><a href="<?php echo base_url()?>index.php/cart/removecart/<?php echo $item['rowid'];?>">X</a></h5>
+                    <span class="hidden" id="itemid_<?php echo $i;?>"><?php echo $item['rowid'];?></span>
+                </div>
+                <div class="col-md-2 col-xs-12">
+                    <img src="<?php echo base_url();?>uploads/product/thumb/<?php echo $details[0]->pimage;?>" class="img-responsive center-block" style="height: 120px;">
+                </div>
+                <div class="col-md-2 col-xs-12 cart-line">
+                    <h5><a href="" style="color: #000;"><?php  echo $details[0]->pname;?> </a></h5>
+                </div>
+                <div class="col-md-2 col-xs-3 cart-line text-center">
+                    <h5>&#8377;  <span id="itemcost_<?php echo $i;?>"><?php echo  $details[0]->cost;?></span></h5>
+                </div>
+            </div>
+            <div style="boder-bottom:solid 1ps #ccc;"></div><?php $i++;}}?>
             <?php foreach ($query as $row ) {
+<<<<<<< HEAD
               $details=$this->user->get_product_by_id($row->productid); 
+=======
+>>>>>>> a42ce7ae0ff93676ddd99421c8798ab472548a06
              ?>
             <div class="col-xs-12 col-md-12  cart-layout cart_item" id="cart_<?php echo $row->id;?>">
                 <div class="col-md-1 col-xs-6 cart-line text-right">
                     <h5><a href="" onclick="javascript:remove_cart(<?php echo $row->id;?>);">X</a></h5>
                 </div>
                 <div class="col-md-2 col-xs-12">
-                    <img src="<?php echo base_url();?>uploads/<?php echo $details[0]->picture;?>" class="img-responsive center-block" style="height: 120px;">
+                    <img src="<?php echo base_url();?>uploads/product/thumb/<?php echo $row->pimage;?>" class="img-responsive center-block" style="height: 120px;">
                 </div>
                 <div class="col-md-2 col-xs-12 cart-line">
-                    <h5><a href="" style="color: #000;"><?php  echo $details[0]->title;?></a></h5>
-                    <p><?php echo $row->attributevalue; ?></p>
+                    <h5><a href="" style="color: #000;"><?php  echo $row->pname;?></a></h5>
                 </div>
                 <div class="col-md-2 col-xs-3 cart-line text-center">
-                    <h5>Per Item</h5><h5>&#8377;  <span id="itemcost_<?php echo $row->id;?>"><?php  
+                    <h5>&#8377;  <span id="itemcost_<?php echo $row->id;?>"><?php  
                      
-                    echo $details1[0]->cost;?></span></h5>
-                </div>
-                <div class="col-md-3 col-xs-6 cart-line" style="padding-top:4%;">
-                    <div class="text-center quantity"><p>Qty</p>
-                        <input  step="1" min="1" max="" name="quantity" value="<?php echo $row->item;?>" title="Qty" class="input-text qty text text-center" size="4" pattern="[0-9]*" inputmode="numeric" id="itemno_<?php echo $row->id;?>" onchange="javascript:item(<?php echo $row->id;?>);" >
-                    </div>
-                </div>
-                <div class="col-md-2 col-xs-3 cart-line text-center">
-                    <h5>Total</h5><h5>&#8377; <span id="cost_<?php echo $row->id;?>"><?php echo  $details1[0]->cost*$row->item;?></span></h5>
+                    echo $row->cost;?></span></h5>
                 </div>
             </div><div style="boder-bottom:solid 1ps #ccc;"></div>
             <?php }?>
@@ -46,32 +57,40 @@
                         <td class="text-right"><h5><strong>&#8377;
                   <span id="totalcost" >
                     <?php $i=0;
-                    if(!empty($this->session->userdata('uid'))){
+                  if(!empty($this->cart->contents()))
+                    { foreach ($cart as $item )
+                        {$details=$this->user->showproduct1($item['id']);
+                            $i=$i+($details[0]->cost*$item['qty']);
+                        }
+                    }
+                    elseif(!empty($this->session->userdata('uid'))){
                    foreach ($query as $row ) {
-                  $details=$this->user->get_product_by_id($row->productid);
-                   $i=$i+($details1[0]->cost*$row->item);}}
+                   $i=$i+($row->cost*$row->item);}}
                    echo $i;?></span></strong></h5></td>
-                    </tr>
-                    <tr>
-                        
-                        <td><h5>Estimated shipping</h5></td>
-                        <td class="text-right"><h5><strong>0</strong></h5></td>
                     </tr>
                     <tr>
                         
                         <td><h3>Total</h3></td>
                         <td class="text-right"><h3><strong>&#8377;
-                  <span id="totalcost1" ><?php $i=0;
-                  if(!empty($this->session->userdata('uid'))){
+                  <span id="totalcost1" >
+                    <?php $i=0;
+                  if(!empty($this->cart->contents()))
+                    { foreach ($cart as $item )
+                        {$details=$this->user->showproduct1($item['id']);
+                            $i=$i+($details[0]->cost*$item['qty']);
+                        }
+                    }
+                    elseif(!empty($this->session->userdata('uid'))){
                    foreach ($query as $row ) {
-                   $i=$i+($details1[0]->cost*$row->item);}}
-                   echo $i;?></span></strong></h3></td>
+                   $i=$i+($row->cost*$row->item);}}
+                   echo $i;?>
+                    </span></strong></h3></td>
                     </tr>
                 </tbody>
             </table>
             <?php if($i!='0'){?>
-            <a class="btn col-xs-12 col-md-12 btn-lg theme-btn" href="<?php echo base_url(); ?>index.php/checkout" style="" > PROCEED</a><?php } else{?>
-            <a class="btn col-xs-12 col-md-12 btn-lg theme-btn" href="<?php echo base_url(); ?>" style="" >CONTINUE SHOPPING</a><?php }?>
+            <a class="btn col-xs-12 col-md-12 btn-lg btn-primary" href="<?php echo base_url(); ?>index.php/checkout" style="" > PROCEED</a><?php } else{?>
+            <a class="btn col-xs-12 col-md-12 btn-lg btn-primary" href="<?php echo base_url(); ?>" style="" >CONTINUE SHOPPING</a><?php }?>
         </div>
         </div>
         </div>
