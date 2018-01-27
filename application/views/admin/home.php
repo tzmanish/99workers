6ba67f049eeba9dcd3e573fa7e3d6583e31eeca1
AMIT ANAND <amitanand.kvj.2@gmail.com>
2018-01-27 22:28:36 +0530

    e
    ew

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -16,7 +16,7 @@
           <div class="card text-white bg-warning o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
-                <i class="fa fa-fw fa-list"></i>
+              	<i class="fa fa-check-square-o" aria-hidden="true"></i>
               </div>
               <div class="mr-5">COMPLEATED PROJECTS:<?php $details=$this->user1->comprojectcount(); if($details){echo $details; }else{echo "0";}?></div>
             </div>
@@ -26,7 +26,7 @@
           <div class="card text-white bg-success o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
-                <i class="fa fa-fw fa-shopping-cart"></i>
+                <i class="fa fa-dot-circle-o" aria-hidden="true"></i>
               </div>
               <div class="mr-5">ONGOING PROJECTS:<?php  $details=$this->user1->onprojectcount(); if($details){echo $details; }else{echo "0";}?></div>
             </div>
@@ -36,9 +36,9 @@
           <div class="card text-white bg-danger o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
-                <i class="fa fa-fw fa-support"></i>
+                <i class="fa fa-credit-card" aria-hidden="true"></i>
               </div>
-              <div class="mr-5">TOTAL PROJECT WORTH:<?php $i=0; foreach($query2 as $row){if($row->pstatus=="0" || $row->pstatus=="1") { $i=$i+$row->cost;} }echo $i;?></div>
+              <div class="mr-5">TOTAL PROJECT WORTH: $<?php $i=0; foreach($query2 as $row){if($row->pstatus=="0" || $row->pstatus=="1") { $i=$i+$row->cost;} }echo $i;?></div>
             </div>
           </div>
         </div>
@@ -46,9 +46,9 @@
           <div class="card text-white bg-danger o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
-                <i class="fa fa-fw fa-support"></i>
+              	<i class="fa fa-credit-card" aria-hidden="true"></i>
               </div>
-              <div class="mr-5">PAYMENT COLLECTED: <?php $j=0; foreach($query1 as $row){if($row->payment_status=="Completed") {$j=$j+$row->payment_gross;}} echo $j;?></div>
+              <div class="mr-5">PAYMENT COLLECTED: $<?php $j=0; foreach($query1 as $row){if($row->payment_status=="Completed") {$j=$j+$row->payment_gross;}} echo $j;?></div>
             </div>
           </div>
         </div>
@@ -56,9 +56,9 @@
           <div class="card text-white bg-danger o-hidden h-100">
             <div class="card-body">
               <div class="card-body-icon">
-                <i class="fa fa-fw fa-support"></i>
+              	<i class="fa fa-credit-card" aria-hidden="true"></i>
               </div>
-              <div class="mr-5">PAYMENT DUE:<?php echo $k=$i-$j;?></div>
+              <div class="mr-5">PAYMENT DUE: $<?php echo $k=$i-$j;?></div>
             </div>
           </div>
         </div>
