f18b16e99cef486b731c5d9751303092621acfc6
manish <tzmanish97@gmail.com>
2018-01-28 19:35:34 +0530

    yhfj

--- application/models/User1.php
+++ application/models/User1.php
@@ -350,11 +350,9 @@ class user1 extends CI_Model
 	}
 	function due($pid)
 	{ 
-<<<<<<< HEAD
 		
 		$this->db->where('user_id',$uid);
-=======
->>>>>>> 65d4f3f0fc282894ac91541438c7f683ab3c2824
+
 		$this->db->where('product_id',$pid);
 		$query=$this->db->get('payments');
 		return $query->result();
